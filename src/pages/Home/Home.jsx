9397e477e5a5f94ab067808a3c576355b6d0f43e
Shailendra Pratap Singh <crownshivam68@gmail.com>
2025-02-24 14:45:37 +0530

    home page build

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Home.css'
+import Navbar from '../../components/Navbar/Navbar'
+import hero_banner from '../../../public/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
+import TitleCards from '../../components/TitleCards/TitleCards'
+const Home = () => {
+  return (
+    <div className='home'>
+      <Navbar/>
+      <div className="hero">
+        <img src={hero_banner} alt=""  className='hero-img'/>
+        <div className="hero-caption">
+            <img src={hero_title} alt="" className='caption-img'/>
+            <p>Discovering his ties to a secret ancient order,a young man living in modern Istambul embarks on a quest to save the city from an immortal enemy.</p>
+            <div className="hero-btns">
+                <button className='btn'><img src={play_icon} alt="" />Play</button>
+                <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
+            </div>
+            <TitleCards/>
+        </div>
+       
+      </div>
+      
+    </div>
+  )
+}
+
+export default Home
